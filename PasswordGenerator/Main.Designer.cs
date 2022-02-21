f5908b2201771cd410c1a529aae3e840f7f003de
varti <varti@VARTI>
2022-02-21 12:01:53 +0200

    Add project files.

---
+++ PasswordGenerator/Main.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace PasswordGenerator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(219, 41);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(75, 23);
+            this.GenerateButton.TabIndex = 0;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(12, 12);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(481, 23);
+            this.PasswordTextBox.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 72);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.GenerateButton);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button GenerateButton;
+        private TextBox PasswordTextBox;
+    }
+}
---
+++ PasswordGenerator/Main.cs
@@ -0,0 +1,27 @@
+﻿namespace PasswordGenerator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            var parts = 7;
+            var partlen = 7;
+            var numberOfNonAlphanumericCharacters = 2;
+
+            var password = string.Empty;
+
+            for (int i = 0; i < parts; i++)
+            {
+                var passwordPart = Password.Generate(partlen, numberOfNonAlphanumericCharacters);
+                password += passwordPart + "-";
+            }
+
+            PasswordTextBox.Text = password;
+        }
+    }
+}
